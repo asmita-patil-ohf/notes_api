ce7b52cd0b66552e814a9568b6b3d6be91cdfa34
Asmita Patil <1646asmitapatil@gmail.com>
2026-03-05 16:29:46 +0530

    Added DELETE method

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get("/notes/:id", (req, res) => {
     if (!note) {
         return res.status(404).json({ message: "Note not found" });
     }
+    res.json(note);
 });
 
 app.post("/notes",(req,res) => {
@@ -34,7 +35,25 @@ app.post("/notes",(req,res) => {
     notes.push(newNote);
     fs.writeFileSync("notes.json", JSON.stringify(notes,null,2));
     res.status(201).json({message: "Note added successfully!"});
-})
+});
+
+app.delete("/notes/:id",(req,res)=>{
+    const noteId= parseInt(req.params.id);
+
+    const data = fs.readFileSync("notes.json");
+    const notes=JSON.parse(data);
+
+    const filteredNotes = notes.filter(n => n.id !== noteId);
+
+    if (notes.length === filteredNotes.length) {
+        return res.status(404).json({ message: "Note not found" });
+    }
+
+    fs.writeFileSync("notes.json",JSON.stringify(filteredNotes, null, 2));
+
+    res.json({ message: "Note deleted successfully" });
+
+});
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
